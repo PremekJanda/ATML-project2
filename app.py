03cdad8c296781abead3d36aecdf0b83e57cb828
PremekJanda <xjanda28@vutbr.cz>
2024-01-07 09:50:10 +0100

    Send speech data

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request, jsonify
-from flask_socketio import SocketIO
+import os, librosa, audioread, uuid, wave
+from pydub import AudioSegment
+from pydub.effects import normalize
+from flask import Flask, render_template, jsonify, send_file, url_for
+from flask import Flask, request, session
+from flask_socketio import SocketIO, emit
+from scipy.io.wavfile import read
+
+audioread.ffdec.FFmpegAudioFile = audioread.ffdec.FFmpegAudioFile
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SECRET_KEY'] = 'secret!'
+app.config['UPLOAD_FOLDER'] = 'static/audio'
 socketio = SocketIO(app)
 
 @app.route('/')
@@ -17,16 +25,83 @@ def handle_message(msg):
 def send_response():
     content = request.json
     user = content.get('user', 'User')
-    message = content.get('message', '')
+    message: str = content.get('message', '')
     
-    #### THIS IS WHERE MAGIC HAPPENS
+    #### THIS IS WHERE MAGIC HAPPENS ###
     print(message)
 
     if message:
         socketio.emit('message', {'user': user, 'msg': message})
-        return jsonify({'success': True, 'message': 'Message sent successfully'})
+        return jsonify({'success': True, 'message': message.upper()})
     else:
-        return jsonify({'success': False, 'message': 'Message cannot be empty'})
+        return jsonify({'success': False, 'message': message.upper()})
+
+@app.route('/process_audio', methods=['POST'])
+def process_audio():
+     # Check if the POST request has a file part
+    if 'audio' not in request.files:
+        return 'No audio file provided', 400
+
+    audio_file = request.files['audio']
+    print(request.form['name'])
+
+    if audio_file:
+        # Save the uploaded audio file
+        audio_file_path = os.path.join(app.config['UPLOAD_FOLDER'], f"input-{request.form['name']}.webm")
+        audio_file.save(audio_file_path)
+
+        # Load the audio file using pydub
+        # audio = librosa.load(audio_file_path, sr=48000)
+        # audio = librosa.load(audio_file_path)
+        audio = read(audio_file_path)
+        # audio = AudioSegment.from_wav(audio_file_path)
+
+        # Perform some processing (e.g., noise removal)
+        processed_audio = normalize(audio)
+
+        # Save the processed audio file
+        processed_audio_path = os.path.join(app.config['UPLOAD_FOLDER'], f"processed-{request.form['name']}.webm")
+        processed_audio.export(processed_audio_path, format='wav')
+
+        # Send the processed audio file back to the client
+        return send_file(processed_audio_path, as_attachment=True)
+
+# # file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+
+# @app.route('/process_audio', methods=['POST'])
+# def process_audio():
+#     audio_file = request.files['audio']
+
+#     if audio_file:
+#         # audio = AudioSegment.from_file(audio_file, format='wav')
+#         # audio = librosa.load(audio_file)
+#         # audio = read(audio_file)
+#         print(audio_file)
+#         # with open("", "r") as f:
+#         #     f.read()
+#         # try:
+#         audio = AudioSegment.from_file(audio_file.read(), format='webm')
+#         audio.export("output.wav", format="wav")
+#         print(audio)
+
+#         # Perform noise removal (you can customize this step)
+#         processed_audio = normalize(audio)
+
+#         # Save the processed audio file
+#         processed_audio_path = 'static/processed_audio.wav'
+#         processed_audio.export(processed_audio_path, format='wav')
+        
+#         # Send the processed audio file back to the client
+#         return send_file(processed_audio_path, as_attachment=True)
+#         # except Exception as e:
+#         #     print(f"Error: {e}")
+
+#         # return send_file(audio_file, 
+#         #                  as_attachment=True, 
+#         #                  mimetype=audio_file.mimetype,
+#         #                  download_name=audio_file.name)
+
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
--- static/app.js
+++ static/app.js
@@ -1,5 +1,3 @@
-console.log("js loaded");
-
 document.addEventListener('DOMContentLoaded', function() {
     document.getElementById('messageInput').focus();
     document.getElementById('questionForm').addEventListener('submit', function(event) {
@@ -10,23 +8,25 @@ document.addEventListener('DOMContentLoaded', function() {
     
 });
 
-function displayMessage(msg, type) {
+function displayMessage(msg, type, is_audio=false) {
     var chatContainer = document.getElementById('chat-container');
     var newMessage = document.createElement("div");
-    
-    if (type == "request") {
-
-    } else if (type == "response") {
-        
-    }
     newMessage.classList.add("message", type)
-    newMessage.innerHTML = '<strong>' + msg.user + ':</strong> ' + msg.msg;
+    
+    if (is_audio) {
+        let strong = document.createElement("strong")
+        strong.innerHTML = msg.user
+        newMessage.appendChild(strong);
+        newMessage.appendChild(msg.msg);
+    } else {
+        newMessage.innerHTML = '<strong>' + msg.user + '</strong> ' + `${msg.msg}`;
+    }
+
     chatContainer.appendChild(newMessage);
     chatContainer.scrollTop = chatContainer.scrollHeight;
 }
 
 function sendMessage() {
-    
     var messageInput = document.getElementById('messageInput');
     var message = messageInput.value.trim();
 
@@ -41,7 +41,7 @@ function sendMessage() {
         .then(response => response.json())
         .then(data => {
             if (data.success) {
-                displayMessage({'user': 'User', 'msg': message}, "response");
+                displayMessage({'user': 'ChatBot', 'msg': data.message}, "response");
                 messageInput.value = '';
             } else {
                 console.error('Failed to send message:', data.message);
@@ -50,3 +50,311 @@ function sendMessage() {
         .catch(error => console.error('Error:', error));
     }
 }
+
+function sendAudio(audioBlob) {
+    const formData = new FormData();
+    formData.append('audio', audioBlob);    
+    formData.append('name', getNewId());
+
+    fetch('/process_audio', {
+        method: 'POST',
+        body: formData
+    })
+    .then(response => response.blob())
+    .then(blob => {
+        // processedAudio.src = URL.createObjectURL(blob);
+        createAudio(blob, "ChatBot", "response")
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+
+let preview = document.getElementById("preview");
+let recording = document.getElementById("recording");
+let startButton = document.getElementById("startButton");
+let stopButton = document.getElementById("stopButton");
+let downloadButton = document.getElementById("downloadButton");
+
+function log(msg) {
+    document.getElementById("log").innerHTML += `${msg}\n`;
+}
+
+function toggleButtons() {
+  if (startButton.style.display == "none") {
+    startButton.style.display = "inline-block"
+    stopButton.style.display = "none"
+  } else {
+    stopButton.style.display = "inline-block"
+    startButton.style.display = "none"
+  }
+}
+
+function getNewId(start="audio-id-") {
+    const elements = document.querySelectorAll('[id^="'+start+'"]');
+    return start+elements.length;
+}
+
+function createAudio(recordedChunks, user="User", type="request") {
+    const audioElement = document.createElement('audio');
+    const audioBlob = new Blob(recordedChunks, { type: "audio/webm" })
+    audioElement.src = URL.createObjectURL(audioBlob);
+    audioElement.controls = true;
+    audioElement.id = getNewId();
+    audioElement.download = audioElement.id + ".webm";
+    msg = {'user': user, 'msg': audioElement};
+    displayMessage(msg, type, is_audio=true);
+    return audioBlob;
+}
+
+// function createAudio(recordedChunks, user="User", type="request") {
+//     const audioElement = document.createElement('audio');
+//     const audioBlob = recordedChunks
+//     audioElement.src = URL.createObjectURL(recordedChunks);
+//     audioElement.controls = true;
+//     audioElement.id = getNewId();
+//     audioElement.download = audioElement.id + ".webm";
+//     msg = {'user': user, 'msg': audioElement};
+//     displayMessage(msg, type, is_audio=true);
+//     return audioBlob;
+// }
+
+async function startRecording(stream) {
+    let recorder = new MediaRecorder(stream);
+    let data = [];
+
+    recorder.ondataavailable = (event) => data.push(event.data);
+    recorder.start();
+    toggleButtons();
+
+    let stopped = new Promise((resolve, reject) => {
+        recorder.onstop = resolve;
+        recorder.onerror = (event) => reject(event.name);
+    });
+
+    let recorded = new Promise(resolve => {
+        stopButton.addEventListener("click", resolve, { once: true });
+    })
+    .then(() => {
+        if (recorder.state === "recording") {
+          recorder.stop();
+          stop(preview.srcObject);
+          toggleButtons();
+        }
+    });  
+
+    await Promise.all([stopped, recorded]);
+    return data;
+}
+
+function stop(stream) {
+  stream.getTracks().forEach((track) => track.stop());
+}
+
+startButton.addEventListener("click", () => {
+    navigator.mediaDevices
+        .getUserMedia({
+            video: false,
+            audio: true,
+        })
+        .then((stream) => {
+            preview.srcObject = stream;
+            downloadButton.href = stream;
+            preview.captureStream = preview.captureStream || preview.mozCaptureStream;
+            return new Promise((resolve) => (preview.onplaying = resolve));
+        })
+        .then(() => startRecording(preview.captureStream()))
+        .then((recordedChunks) => {
+            const audioBlob = createAudio(recordedChunks);
+            sendAudio(audioBlob);
+        })
+        .catch((error) => {
+            if (error.name === "NotFoundError") {
+                log("Microphone not found. Can't record.");
+            } else {
+                log(error);
+            }
+        });
+  }, false,
+);
+
+// startButton.addEventListener("click", () => {
+//     navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+//         // Collection for recorded data.
+//         let data = [];
+      
+//         // Recorder instance using the stream.
+//         // Also set the stream as the src for the audio element.
+//         const recorder = new MediaRecorder(stream);
+//         preview.srcObject = stream;
+//         recording.srcObject = stream;
+      
+//         recorder.addEventListener('start', e => {
+//           // Empty the collection when starting recording.
+//           data.length = 0;
+//         });
+      
+//         recorder.addEventListener('dataavailable', event => {
+//           // Push recorded data to collection.
+//           data.push(event.data);
+//         });
+      
+//         // Create a Blob when recording has stopped.
+//         recorder.addEventListener('stop', () => {
+//           const blob = new Blob(data, {
+//             'type': 'audio/mp3'
+//           });
+//           sendAudio(blob);
+//         });
+      
+//         // Start the recording.
+//         // recorder.start();
+//       });
+// })
+
+// stopButton.addEventListener("click", () => {
+//         stop(preview.srcObject);
+//         toggleButtons();
+//     }, false,
+// );
+
+
+
+
+
+
+// ! 2
+// let mediaRecorder;
+// let audioChunks = [];
+// let audioBlob;
+
+// const startRecordButton = document.getElementById('startRecord');
+// const stopRecordButton = document.getElementById('stopRecord');
+// const sendToServerButton = document.getElementById('sendToServer');
+// const audioPlayer = document.getElementById('audioPlayer');
+
+// navigator.mediaDevices.getUserMedia({ audio: true })
+//     .then(stream => {
+//         mediaRecorder = new MediaRecorder(stream);
+
+//         mediaRecorder.ondataavailable = event => {
+//             if (event.data.size > 0) {
+//                 audioChunks.push(event.data);
+//             }
+//         };
+
+//         mediaRecorder.onstop = () => {
+//             audioBlob = new Blob(audioChunks, { type: 'audio/webm' });
+//             const audioUrl = URL.createObjectURL(audioBlob);
+
+//             audioPlayer.src = audioUrl;
+//             audioPlayer.controls = true;
+
+//             sendToServerButton.disabled = false;
+//         };
+
+//         startRecordButton.addEventListener('click', () => {
+//             mediaRecorder.start();
+//             startRecordButton.disabled = true;
+//             stopRecordButton.disabled = false;
+//         });
+
+//         stopRecordButton.addEventListener('click', () => {
+//             mediaRecorder.stop();
+//             startRecordButton.disabled = false;
+//             stopRecordButton.disabled = true;
+//         });
+
+//         sendToServerButton.addEventListener('click', () => {
+//             const formData = new FormData();
+//             // formData.append('audio', new File(audioChunks, 'recorded_audio.wav'));
+//             formData.append('audio', audioBlob);
+
+//             fetch('/process_audio', {
+//                 method: 'POST',
+//                 body: formData
+//             })
+//             .then(response => response.blob())
+//             .then(blob => {
+//                 const processedAudioUrl = URL.createObjectURL(blob);
+//                 audioPlayer.src = processedAudioUrl;
+//                 audioPlayer.controls = true;
+//             })
+//             .catch(error => console.error('Error:', error));
+//         });
+//     })
+//     .catch(error => console.error('Error accessing microphone:', error));
+
+
+
+
+// ! 3
+// let mediaRecorder;
+// let audioChunks = [];
+// let audioBlob;
+
+// const startRecordButton = document.getElementById('startRecord');
+// const stopRecordButton = document.getElementById('stopRecord');
+// const sendToServerButton = document.getElementById('sendToServer');
+// const audioPlayer = document.getElementById('audioPlayer');
+// var base64data = 0;
+// var reader;
+// var recorder, gumStream;
+// var recordButton = my_btn;
+// reader = new FileReader();
+// reader.readAsDataURL(e.data); 
+// reader.onloadend = function() {
+//     base64data = reader.result;
+//     //console.log("Inside FileReader:" + base64data);
+// }
+
+// navigator.mediaDevices.getUserMedia({ audio: true })
+//     .then(stream => {
+//         mediaRecorder = new MediaRecorder(stream);
+
+//         mediaRecorder.ondataavailable = event => {
+//             if (event.data.size > 0) {
+//                 audioChunks.push(event.data);
+//             }
+//         };
+
+//         mediaRecorder.onstop = () => {
+//             audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
+//             const audioUrl = URL.createObjectURL(audioBlob);
+
+//             audioPlayer.src = audioUrl;
+//             audioPlayer.controls = true;
+
+//             sendToServerButton.disabled = false;
+//         };
+
+//         startRecordButton.addEventListener('click', () => {
+//             mediaRecorder.start();
+//             startRecordButton.disabled = true;
+//             stopRecordButton.disabled = false;
+//         });
+
+//         stopRecordButton.addEventListener('click', () => {
+//             mediaRecorder.stop();
+//             startRecordButton.disabled = false;
+//             stopRecordButton.disabled = true;
+//         });
+
+//         sendToServerButton.addEventListener('click', () => {
+//             const formData = new FormData();
+//             // formData.append('audio', new File(audioChunks, 'recorded_audio.wav'));
+//             formData.append('audio', audioBlob);
+
+//             fetch('/process_audio', {
+//                 method: 'POST',
+//                 body: formData
+//             })
+//             .then(response => response.blob())
+//             .then(blob => {
+//                 const processedAudioUrl = URL.createObjectURL(blob);
+//                 audioPlayer.src = processedAudioUrl;
+//                 audioPlayer.controls = true;
+//             })
+//             .catch(error => console.error('Error:', error));
+//         });
+//     })
+//     .catch(error => console.error('Error accessing microphone:', error));
